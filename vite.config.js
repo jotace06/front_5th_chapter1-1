e2356186314bc43feedba26fc27e69154642df24
JC Chung <jc@JCs-MacBook-Pro.local>
2025-03-27 18:09:50 +0900

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  base: "/front_5th_chapter1-1/", // GitHub repository 이름
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        hash: "index.hash.html",
+export default defineConfig(({ command }) => {
+  const isProduction = command === "build";
+
+  return {
+    base: isProduction ? "/front_5th_chapter1-1/" : "/",
+    build: {
+      outDir: "dist",
+      rollupOptions: {
+        input: {
+          main: "index.html",
+          hash: "index.hash.html",
+        },
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    },
+  };
 });
