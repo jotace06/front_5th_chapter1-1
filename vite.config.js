4c2393bf84a6a6a1cb9c4e4924f9a04ed4e53af5
JC Chung <jc@JCs-MacBook-Pro.local>
2025-03-27 17:00:53 +0900

    add deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/front_5th_chapter1-1/", // GitHub repository 이름
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        hash: "index.hash.html",
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
